7bb7429e14c5e1db09eb8a02f8057a3c591a31eb
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-23 13:45:35 +0400

    [add] root component with Provider

---
+++ src/components/Root.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import App from './App';
+
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+};
+
+export default Root;
