023f9a2650915b746cd935d387093664d4232b76
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-25 01:04:52 +0400

    [add] new actions for getting movies

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,57 @@
-import { LOGIN_SUCCESS } from './constants';
+import {
+  LOGIN_SUCCESS,
+  FAILURE_MOVIES,
+  RECEIVE_MOVIES,
+  REQUEST_MOVIES,
+  SELECT_GENRE,
+} from './constants';
 
 const loginSuccess = user => ({
   type: LOGIN_SUCCESS,
   payload: user,
 });
 
-export { loginSuccess, LOGIN_SUCCESS };
+const requestMovies = (genre, page = 1) => {
+  return dispatch => {
+    dispatch(selectGenre);
+    dispatch({ type: REQUEST_MOVIES });
+
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=e8e227add2a2e5c168f7c3845928d8db&language=en-US&page=${page}`
+    )
+      .then(r => r.json())
+      .then(data => dispatch(receiveMovies(genre, data)))
+      .catch(err => dispatch(failureMovies(err)));
+  };
+};
+
+requestMovies();
+
+const selectGenre = genre => ({
+  type: SELECT_GENRE,
+  payload: {
+    selectedGenre: genre,
+  },
+});
+
+const failureMovies = err => ({
+  type: FAILURE_MOVIES,
+  err,
+});
+
+const receiveMovies = (selectedGenre, movies) => ({
+  type: RECEIVE_MOVIES,
+  payload: {
+    selectedGenre,
+    movies,
+  },
+});
+
+export {
+  loginSuccess,
+  failureMovies,
+  receiveMovies,
+  selectGenre,
+  requestMovies,
+  LOGIN_SUCCESS,
+};
