fd1ad0372760328d976f3108477b52690c819323
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-24 21:25:48 +0400

    [add] validator

--- src/helpers/validate.js
+++ src/helpers/validate.js
@@ -1,10 +1,18 @@
 export default function(values) {
   const errors = {};
   const requiredFields = ['firstName', 'password'];
+
   requiredFields.forEach(field => {
     if (!values[field]) {
-      errors[field] = 'Required';
+      errors[field] = "You doesn't fill required field";
+    }
+
+    if (values[field] && values[field].length < 5) {
+      errors[field] = 'Your input must contain more than 5 symbols!';
+    } else if (values[field] && values[field].trim().length < 5) {
+      errors[field] = "You're trying to input spaces? Go Home!";
     }
   });
+
   return errors;
 }
