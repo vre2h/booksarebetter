80112d474bd7a5af0b0ffac50e5ae60f60d43e1a
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-25 01:05:49 +0400

    [upd] reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { reducer as reduxFormReducer } from 'redux-form';
 import currentUser from './currentUser';
 import isAuth from './isAuth';
+import movies from './getMovies';
 
 export default combineReducers({
   form: reduxFormReducer,
   isAuth,
   currentUser,
+  movies,
 });
