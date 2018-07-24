077f863e23177ceeb8a40af8f65736feab51b194
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-24 21:25:57 +0400

    [add] styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Root from './components/Root';
+import Root from './components/Root/';
 import configureStore from './store/configureStore.dev';
 import 'normalize.css';
+import './index.css';
 
 const store = configureStore();
 
