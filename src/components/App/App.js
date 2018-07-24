abb90c637b6999a1cb4db0755985a9f5b344d7f6
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-24 21:26:36 +0400

    [add] app with minimal router

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import LoginForm from '../../containers/LoginForm';
+import ProtectedRouteContainer from '../../containers/ProtectedRouteContainer';
+
+const App = () => (
+  <React.Fragment>
+    <Switch>
+      <Route exact path="/" component={ProtectedRouteContainer} />
+      <Route path="/login" component={LoginForm} />
+    </Switch>
+  </React.Fragment>
+);
+
+export default App;
