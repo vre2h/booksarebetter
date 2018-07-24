207db2123c1e35916edd0a1ecf380c16ca0c302b
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-24 21:35:33 +0400

    [add] login page

---
+++ src/components/Login/Login.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { Field } from 'redux-form';
+import './styles.css';
+import { Button } from 'react-bootstrap';
+import RenderTextField from './TextField';
+
+class Login extends React.Component {
+  render() {
+    const { from } = this.props.location.state || { from: { pathname: '/' } };
+
+    if (this.props.isAuth) {
+      return <Redirect to={from} />;
+    }
+
+    const { handleSubmit, login, valid } = this.props;
+    const required = value => (value ? undefined : 'Required');
+
+    return (
+      <div className="login-wrapper">
+        <form onSubmit={handleSubmit(login)}>
+          <div className="led-red">Rec</div>
+          <div>
+            <Field
+              name="firstName"
+              component={RenderTextField}
+              id="formInlineName"
+              type="text"
+              label="Text"
+              bsSize="large"
+              placeholder="Login"
+              validate={[required]}
+            />
+          </div>
+          <div>
+            <Field
+              name="password"
+              component={RenderTextField}
+              id="formControlsPassword"
+              type="password"
+              label="Password"
+              placeholder="Password"
+              bsSize="large"
+            />
+          </div>
+          <div>
+            <Button
+              bsStyle="primary"
+              bsSize="large"
+              block
+              type="submit"
+              disabled={!valid}
+            >
+              Log In
+            </Button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
