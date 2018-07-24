3a0cb72fd19eaa86dc2c8bdf10c87a123e732d5e
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-24 23:35:03 +0400

    [delete] mui provider

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -2,18 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { MuiThemeProvider } from 'material-ui/styles';
 import App from '../App';
 import './styles.css';
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <Router>
-      <MuiThemeProvider>
-        <div className="container">
-          <App />
-        </div>
-      </MuiThemeProvider>
+      <div className="container">
+        <App />
+      </div>
     </Router>
   </Provider>
 );
