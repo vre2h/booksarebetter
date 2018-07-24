6a92f6cf50ea1719ee47c6fd9d8fa39e303c6b6c
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-24 14:48:12 +0400

    [add] validation on login form

--- src/containers/LoginForm.js
+++ src/containers/LoginForm.js
@@ -2,18 +2,23 @@ import { connect } from 'react-redux';
 import { reduxForm } from 'redux-form';
 
 import { loginSuccess } from '../actions';
-
 import Login from '../components/Login';
+import validate from '../helpers/validate';
 
 const mapDispatchToProps = dispatch => ({
   login: user => dispatch(loginSuccess(user)),
 });
 
+const mapStateToProps = state => ({
+  isAuth: state.isAuth,
+});
+
 const LoginForm = reduxForm({
   form: 'loginForm',
+  validate,
 })(Login);
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(LoginForm);
