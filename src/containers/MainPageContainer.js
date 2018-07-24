3b633e886866e186796562c2ce791b7c37880065
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-25 02:08:18 +0400

    [delete] movies from mainPageContainer props

--- src/containers/MainPageContainer.js
+++ src/containers/MainPageContainer.js
@@ -2,17 +2,11 @@ import { connect } from 'react-redux';
 import MainPage from '../components/MainPage/MainPage';
 import { requestMovies } from '../actions';
 
-const mapStateToProps = state => {
-  return {
-    movies: state.movies.moviesByGenre.movies,
-  };
-};
-
 const mapDispatchToProps = dispatch => ({
   fetchData: (...arg) => dispatch(requestMovies(...arg)),
 });
 
 export default connect(
-  mapStateToProps,
+  null,
   mapDispatchToProps
 )(MainPage);
