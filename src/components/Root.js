32ffbd9c27cd031a89904e533e542ffe4920bf4e
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-24 12:25:18 +0400

    [add] material ui and react-router configs

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PropTypes } from 'prop-types';
-import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ProtectedRoute from './ProtectedRoute';
+import { MuiThemeProvider } from 'material-ui/styles';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <App />
+    <Router>
+      <MuiThemeProvider>
+        <Route path="/" component={ProtectedRoute} />
+      </MuiThemeProvider>
+    </Router>
   </Provider>
 );
 
