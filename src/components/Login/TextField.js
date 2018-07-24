9389dd527f744252b74960db3bb971696475a17b
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-24 21:35:17 +0400

    [add] Text Field

---
+++ src/components/Login/TextField.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FormGroup, ControlLabel, FormControl, Alert } from 'react-bootstrap';
+
+const FieldGroup = ({ id, label, touched, error, ...props }) => {
+  return (
+    <FormGroup controlId={id}>
+      <ControlLabel bsClass="login-label">{label}</ControlLabel>
+      <FormControl {...props} />
+      {touched && error ? (
+        <Alert bsStyle="danger" bsClass="login-alert">
+          <strong>Holy guacomole!</strong> {error}
+        </Alert>
+      ) : (
+        ''
+      )}
+    </FormGroup>
+  );
+};
+
+const renderTextField = ({ input, meta: { touched, error }, ...custom }) => (
+  <div>
+    <FieldGroup touched={touched} error={error} {...input} {...custom} />
+  </div>
+);
+
+export default renderTextField;
