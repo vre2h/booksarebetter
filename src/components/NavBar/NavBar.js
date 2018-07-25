efcb51590729fe0334a7843ce1e21598ca17cc41
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-26 01:23:44 +0400

    [add] logout btn

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,10 +3,14 @@ import { FormControl } from 'react-bootstrap';
 import './styles.css';
 
 const NavBar = props => {
+  const { logOut } = props;
   return (
     <div className="nav-bar">
       <div className="nav-bar__title">Books are better</div>
       <FormControl bsClass="nav-bar__search" type="text" placeholder="Search" />
+      <button onClick={logOut} className="nav-bar__logout">
+        Log Out
+      </button>
     </div>
   );
 };
