b891edc2c5b959702be239dcc522e0fe7babc66f
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-25 04:08:29 +0400

    [add] navbar to main page

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MoviesContainer from '../../containers/MoviesContainer';
+import NavBar from '../NavBar';
 
 class MainPage extends React.Component {
   componentDidMount() {
@@ -7,7 +8,12 @@ class MainPage extends React.Component {
   }
 
   render() {
-    return <MoviesContainer />;
+    return (
+      <div>
+        <NavBar />
+        <MoviesContainer />
+      </div>
+    );
   }
 }
 
