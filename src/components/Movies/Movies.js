e9500860294290a9ab64ec8df0abb9ea1622224b
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-26 04:08:29 +0400

    [add] genres to movie list

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Movies = ({ movies }) => {
-  return movies.map(movie => {
-    const imgUrl = `https://image.tmdb.org/t/p/w200/${movie.poster_path}`;
+const Movies = ({ movies, genresById }) => {
+  return movies.map(({ id, title, genre_ids, poster_path }) => {
+    const imgUrl = `https://image.tmdb.org/t/p/w200/${poster_path}`;
 
     return (
-      <div key={movie.id}>
-        <p>{movie.title}</p>
+      <div key={id}>
+        <p>{title}</p>
         <img src={imgUrl} alt="Sorry, we can't upload!" />
+        <ul>{genre_ids.map(id => <li>{genresById[id]}</li>)}</ul>
       </div>
     );
   });
