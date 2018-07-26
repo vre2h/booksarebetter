8b5fd231f1503dc7188af3b0552e5a68bfaa507f
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-26 17:22:30 +0400

    [add] movies style

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Movie from '../Movie';
-const Movies = ({ movies, genresById }) => {
+import './styles.css';
+
+const Movies = ({ movies, genresById, genreName }) => {
   return (
     <div className="movies-wrapper">
-      {' '}
-      {movies.map(movieInfo => {
-        return <Movie movieInfo={movieInfo} genresById={genresById} />;
-      })}
+      <h1 className="movies-title">{genreName}</h1>{' '}
+      <div className="movies">
+        {movies.map(movieInfo => {
+          return (
+            <Movie
+              key={movieInfo.id}
+              movieInfo={movieInfo}
+              genresById={genresById}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
