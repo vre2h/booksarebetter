ca4ffcb466274ab4cff8bd7052f51a2a965566b6
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-26 17:21:58 +0400

    [add] styles for Movie

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Badge } from 'react-bootstrap';
+import './styles.css';
 
 const Movie = props => {
   const { movieInfo, genresById } = props;
@@ -6,10 +8,20 @@ const Movie = props => {
   const imgUrl = `https://image.tmdb.org/t/p/w200/${poster_path}`;
 
   return (
-    <div key={id}>
-      <p>{title}</p>
-      <img src={imgUrl} alt="Sorry, we can't upload!" />
-      <ul>{genre_ids.map(id => <li key={id}>{genresById[id]}</li>)}</ul>
+    <div key={id} className="movie">
+      <div className="movie-shadow" />
+      <img className="movie-img" src={imgUrl} alt="Sorry, we can't upload!" />
+      <div className="movie-desc">
+        {' '}
+        <p className="movie-title">{title}</p>
+        <ul className="movie-genres">
+          {genre_ids.map(id => (
+            <li className="movie-genres-item" key={id}>
+              <Badge>{genresById[id]}</Badge>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
