045b5be933997a02b137a35ca15e2f73252fbc28
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-28 02:00:39 +0400

    [add] add to favs from popular page

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -3,34 +3,55 @@ import { Link } from 'react-router-dom';
 import { Badge, Button, Glyphicon } from 'react-bootstrap';
 import './styles.css';
 
-const Movie = props => {
-  const { movieInfo, genresById } = props;
-  const { id, title, genre_ids, poster_path } = movieInfo;
-  const imgUrl = `https://image.tmdb.org/t/p/w200/${poster_path}`;
+class Movie extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <Link to={`movies/movie/${id}`}>
-      <div className="movie">
-        <div className="movie-top-overlay">
-          <Button bsSize="small">
-            <Glyphicon glyph="star" />
-          </Button>
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(id, e) {
+    e.preventDefault();
+
+    const { addFav, removeFav, isFav } = this.props;
+    const toggler = isFav ? removeFav : addFav;
+
+    toggler();
+  }
+
+  render() {
+    const { movieInfo, genresById, isFav } = this.props;
+    const { id, title, genre_ids, poster_path } = movieInfo;
+    const imgUrl = `https://image.tmdb.org/t/p/w200/${poster_path}`;
+
+    return (
+      <Link to={`movies/movie/${id}`}>
+        <div className="movie">
+          <div className="movie-top-overlay">
+            <Button bsSize="small" onClick={this.handleClick.bind(this, id)}>
+              {isFav ? <Glyphicon glyph="minus" /> : <Glyphicon glyph="star" />}
+            </Button>
+          </div>
+          <img
+            className="movie-img"
+            src={imgUrl}
+            alt="Sorry, we can't upload!"
+          />
+          <div className="movie-desc">
+            {' '}
+            <p className="movie-title">{title}</p>
+            <ul className="movie-genres">
+              {genre_ids.map(id => (
+                <li className="movie-genres-item" key={id}>
+                  <Badge>{genresById[id]}</Badge>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <img className="movie-img" src={imgUrl} alt="Sorry, we can't upload!" />
-        <div className="movie-desc">
-          {' '}
-          <p className="movie-title">{title}</p>
-          <ul className="movie-genres">
-            {genre_ids.map(id => (
-              <li className="movie-genres-item" key={id}>
-                <Badge>{genresById[id]}</Badge>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </Link>
-  );
-};
+      </Link>
+    );
+  }
+}
 
 export default Movie;
