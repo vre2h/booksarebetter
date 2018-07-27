c5cb19808b600e12399e3c20b2ee8125b34acde8
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-27 15:57:53 +0400

    [add] movies search container

---
+++ src/containers/MoviesSearchContainer.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Movies from '../components/Movies/Movies';
+import { requestMovies, updSearchField } from '../actions';
+
+const mapStateToProps = state => {
+  return {
+    genresById: state.moviesInfo.genresById,
+    genreName: 'search'.toUpperCase(),
+    isFetching: state.moviesInfo.isFetching,
+    movies: state.moviesInfo.moviesByGenre,
+  };
+};
+
+class MoviesSearchContainer extends React.Component {
+  componentDidMount() {
+    const { searcher } = this.props.match.params;
+    this.props.updSearchField(searcher);
+    this.props.fetchData('search', undefined, searcher, this.props.genresById);
+  }
+
+  render() {
+    const { genresById, genreName, isFetching, movies } = this.props;
+
+    return (
+      <Movies
+        genresById={genresById}
+        genreName={genreName}
+        isFetching={isFetching}
+        movies={movies}
+      />
+    );
+  }
+}
+
+MoviesSearchContainer = connect(
+  mapStateToProps,
+  {
+    fetchData: requestMovies,
+    updSearchField,
+  }
+)(MoviesSearchContainer);
+
+export default MoviesSearchContainer;
