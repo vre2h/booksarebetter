bb587ecd55576ddf71f3b3ecacd695a1c9a7a4a7
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-27 22:43:09 +0400

    [add] action to get Movie

--- src/actions/moviePage.js
+++ src/actions/moviePage.js
@@ -18,15 +18,15 @@ const requestMovie = () => ({
   type: REQUEST_MOVIE,
 });
 
-const fetchMovie = dispatch => id => {
+const fetchMovie = id => dispatch => {
   dispatch(requestMovie());
 
   fetch(
     `https://api.themoviedb.org/3/movie/${id}?api_key=e8e227add2a2e5c168f7c3845928d8db&language=en-US`
   )
     .then(r => r.json())
-    .then(d => receiveMovie(d))
-    .catch(err => failureMovie(err));
+    .then(d => dispatch(receiveMovie(d)))
+    .catch(err => dispatch(failureMovie(err)));
 };
 
 export { fetchMovie };
