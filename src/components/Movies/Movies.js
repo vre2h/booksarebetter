a1f5bb3fcb52ca90c1fcfb45e8339555d916d5a6
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-27 18:11:55 +0400

    [add] loading title on scroll

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -5,28 +5,25 @@ import './styles.css';
 const Movies = ({ movies, genresById, genreName, isFetching }) => {
   return (
     <div className="movies-wrapper">
-      <h1 className="movies-title">{genreName}</h1>{' '}
-      {isFetching ? (
-        <h2>Loading...</h2>
-      ) : (
-        <div className="movies">
-          {movies.length !== 0 ? (
-            movies.map(movieInfo => {
-              return (
-                <Movie
-                  key={movieInfo.id}
-                  movieInfo={movieInfo}
-                  genresById={genresById}
-                />
-              );
-            })
-          ) : (
-            <h3>
-              <strong>Holy guacamole!</strong> There is no post with your input!
-            </h3>
-          )}
-        </div>
-      )}
+      <h1 className="movies-title">{genreName}</h1>
+      <div className="movies">
+        {movies.length !== 0 || isFetching ? (
+          movies.map(movieInfo => {
+            return (
+              <Movie
+                key={movieInfo.id}
+                movieInfo={movieInfo}
+                genresById={genresById}
+              />
+            );
+          })
+        ) : (
+          <h3>
+            <strong>Holy guacamole!</strong> There is no post with your input!
+          </h3>
+        )}
+      </div>
+      {isFetching ? <h2>Loading..</h2> : ''}
     </div>
   );
 };
