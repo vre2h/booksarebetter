6252ef18e588131fbd44628ac09a74598d6b1e77
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-27 20:27:27 +0400

    [add] actions on get single movie

---
+++ src/actions/moviePage.js
@@ -0,0 +1,32 @@
+import { REQUEST_MOVIE, RECEIVE_MOVIE, FAILURE_MOVIE } from './constants';
+
+const failureMovie = err => ({
+  type: FAILURE_MOVIE,
+  err,
+});
+
+const receiveMovie = movie => {
+  return {
+    type: RECEIVE_MOVIE,
+    payload: {
+      movie,
+    },
+  };
+};
+
+const requestMovie = () => ({
+  type: REQUEST_MOVIE,
+});
+
+const fetchMovie = dispatch => id => {
+  dispatch(requestMovie());
+
+  fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=e8e227add2a2e5c168f7c3845928d8db&language=en-US`
+  )
+    .then(r => r.json())
+    .then(d => receiveMovie(d))
+    .catch(err => failureMovie(err));
+};
+
+export { fetchMovie };
