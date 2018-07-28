60239b11f9b0cf759fece3af83d87f8a39b3a34a
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-28 04:12:24 +0400

    [add] favorites container

---
+++ src/containers/FavoritesContainer.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Movies from '../components/Movies/Movies';
+import { fetchFavMovies } from '../actions/favs';
+import { addFav, removeFav } from '../actions/favs';
+
+class FavoritesContainer extends React.Component {
+  componentDidMount() {
+    const { favoriteIds, fetchFavMovies } = this.props;
+
+    fetchFavMovies(favoriteIds);
+  }
+
+  render() {
+    const { movies, genresById, genreName, isFetching } = this.props;
+    return (
+      <div>
+        <Movies
+          movies={movies}
+          genresById={genresById}
+          genreName={genreName}
+          isFetching={isFetching}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  movies: state.favorites.movies || [],
+  favoriteIds: state.favorites.favoriteIds,
+  genresById: state.moviesInfo.genresById || [],
+  genreName: 'favorites'.toUpperCase(),
+  isFetching: state.favorites.isFetching,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchFavMovies: ids => dispatch(fetchFavMovies(ids)),
+  addFav: id => dispatch(addFav(id)),
+  removeFav: id => dispatch(removeFav(id)),
+});
+
+FavoritesContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FavoritesContainer);
+
+export default FavoritesContainer;
