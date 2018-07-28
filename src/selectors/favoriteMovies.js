e46a38154b3e412dc618c313329786b06e647ff7
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-28 20:45:44 +0400

    [del] unsusable selectors

--- src/selectors/favoriteMovies.js
+++
@@ -1,5 +0,0 @@
-import { createSelector } from 'reselect';
-
-export const movies = state => state.favorites.movies;
-
-export default createSelector(movies, movies => movies);
--- src/selectors/isAuth.js
+++
@@ -1,5 +0,0 @@
-import { createSelector } from 'reselect';
-
-export const isAuth = state => state.isAuth;
-
-export default createSelector(isAuth, isAuth => isAuth);
