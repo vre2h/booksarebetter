33da9862423ae89cab1d5ceea4688676cf55c80c
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-31 14:51:10 +0400

    [add] action creators to tests

--- src/__tests__/reducers/currentUser.test.js
+++ src/__tests__/reducers/currentUser.test.js
@@ -1,16 +1,16 @@
 import currentUser from '../../reducers/currentUser';
-import { LOGOUT, LOGIN_SUCCESS } from '../../actions/constants';
+import { logOut, loginSuccess } from '../../actions/login';
 
 test('null on logout', () => {
-  expect(currentUser({}, { type: LOGOUT })).toEqual(null);
+  expect(currentUser({}, logOut())).toEqual(null);
 });
 
 test('userdata on login', () => {
   expect(
-    currentUser(null, {
-      type: LOGIN_SUCCESS,
-      payload: { username: 'username', password: 'password' },
-    })
+    currentUser(
+      null,
+      loginSuccess({ username: 'username', password: 'password' })
+    )
   ).toEqual({ username: 'username', password: 'password' });
 });
 
--- src/__tests__/reducers/favs.test.js
+++ src/__tests__/reducers/favs.test.js
@@ -1,14 +1,16 @@
 import { favoriteIds } from '../../reducers/favs';
-import { ADD_FAV, REMOVE_FAV } from '../../actions/constants';
+import { addFav, removeFav } from '../../actions/favs';
 
 test('add film on fav', () => {
-  expect(favoriteIds([], { type: ADD_FAV, payload: { id: 1 } })).toEqual([1]);
+  expect(favoriteIds([], addFav(1))).toEqual(['1']);
 });
 
 test('remove film on fav', () => {
-  expect(favoriteIds([1, 2], { type: REMOVE_FAV, payload: { id: 2 } })).toEqual(
-    [1]
-  );
+  expect(favoriteIds([1, 2], removeFav(2))).toEqual([1, 2]);
+});
+
+test('return state on non-action', () => {
+  expect(favoriteIds([1, 2, 3], { type: 'WOW' })).toEqual([1, 2, 3]);
 });
 
 test('return state on non-action', () => {
